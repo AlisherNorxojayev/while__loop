13ccec16e4d1017ae5afe8a7f1ca25bf27d3ac19
Alisher <norxojayevalisher3@gmail.com>
2022-08-29 09:50:15 +0500

    done

--- bin/while__loop03.dart
+++ bin/while__loop03.dart
@@ -1,3 +1,6 @@
 void main() {
   //Print your name 2343 times
+  for (int i = 0; i < 2343; i++) {
+    print("Alisher");
+  }
 }
--- bin/while__loop24.dart
+++ bin/while__loop24.dart
@@ -1,3 +1,6 @@
 void main() {
   //Print the numbers from 50 to 30
+  for (int i = 50; i >= 30; i--) {
+    print(i);
+  }
 }
